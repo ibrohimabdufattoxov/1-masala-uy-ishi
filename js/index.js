dde05cd8b968a0b13e7f1cfaf85f649d9db00306
ibrohimabdufattoxov <abdufattakhovibrohim@gmail.com>
2022-05-22 15:41:02 +0500

    init:1-masala

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const money = prompt("Alisher, sizda necha so'm bor")
+alert("Bizga 9642193 ming so'm pul kerak edi")
+
+if(money >=9642193){
+    console.log("Oq yo’l, Alisher!");
+} else{
+    console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan.");
+}
